74e9e70d4ae75323f100d93c6684a77452e2f570
Abrahamcamposr <88513847+Abrahamcamposr@users.noreply.github.com>
2022-06-02 22:15:05 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,169 +1,109 @@
-<meta name="ac-gn-store-key" content="SFX9YPYY9PPXCU9KH" />
-<aside id="ac-gn-segmentbar" class="ac-gn-segmentbar" lang="en-US" dir="ltr" data-strings="{ 'exit': 'Exit', 'view': '{%STOREFRONT%} Store Home', 'segments': { 'smb': 'Business Store Home', 'eduInd': 'Education Store Home', 'other': 'Store Home' } }"></aside>
-<input type="checkbox" id="ac-gn-menustate" class="ac-gn-menustate" />
-<nav id="ac-globalnav" class="no-js" role="navigation" aria-label="Global" data-hires="false" data-analytics-region="global nav" lang="en-US" dir="ltr" data-www-domain="www.apple.com" data-store-locale="us" data-store-root-path="/us" data-store-api="/[storefront]/shop/bag/status" data-search-locale="en_US" data-search-locale="en_US" data-search-suggestions-api="/search-services/suggestions/" data-search-defaultlinks-api="/search-services/suggestions/defaultlinks/">
-	<div class="ac-gn-content">
-		<ul class="ac-gn-header">
-			<li class="ac-gn-item ac-gn-menuicon">
-				<a href="#ac-gn-menustate" role="button" class="ac-gn-menuanchor ac-gn-menuanchor-open" id="ac-gn-menuanchor-open">
-					<span class="ac-gn-menuanchor-label">Global Nav Open Menu</span>
-				</a>
-				<a href="#" role="button" class="ac-gn-menuanchor ac-gn-menuanchor-close" id="ac-gn-menuanchor-close">
-					<span class="ac-gn-menuanchor-label">Global Nav Close Menu</span>
-				</a>
-				<label class="ac-gn-menuicon-label" for="ac-gn-menustate" aria-hidden="true">
-					<span class="ac-gn-menuicon-bread ac-gn-menuicon-bread-top">
-						<span class="ac-gn-menuicon-bread-crust ac-gn-menuicon-bread-crust-top"></span>
-					</span>
-					<span class="ac-gn-menuicon-bread ac-gn-menuicon-bread-bottom">
-						<span class="ac-gn-menuicon-bread-crust ac-gn-menuicon-bread-crust-bottom"></span>
-					</span>
-				</label>
-			</li>
-			<li class="ac-gn-item ac-gn-apple">
-				<a class="ac-gn-link ac-gn-link-apple" href="/" data-analytics-title="apple home" id="ac-gn-firstfocus-small">
-					<span class="ac-gn-link-text">Apple</span>
-				</a>
-			</li>
-			<li class="ac-gn-item ac-gn-bag ac-gn-bag-small" id="ac-gn-bag-small">
-				<div class="ac-gn-bag-wrapper">
-					<a class="ac-gn-link ac-gn-link-bag" href="/us/shop/goto/bag" data-analytics-title="bag" data-analytics-click="bag" aria-label="Shopping Bag" data-string-badge="Shopping Bag with item count :">
-						<span class="ac-gn-link-text">Shopping Bag</span>
-					</a>
-					<span class="ac-gn-bag-badge" aria-hidden="true" data-analytics-title="bag" data-analytics-click="bag">
-						<span class="ac-gn-bag-badge-separator"></span>
-						<span class="ac-gn-bag-badge-number"></span>
-						<span class="ac-gn-bag-badge-unit">+</span>
-					</span>
-				</div>
-				<span class="ac-gn-bagview-caret ac-gn-bagview-caret-large"></span>
-			</li>
-		</ul>
-		<div class="ac-gn-search-placeholder-container" role="search">
-			<div class="ac-gn-search ac-gn-search-small">
-				<a id="ac-gn-link-search-small" class="ac-gn-link" href="/us/search" data-analytics-title="search" data-analytics-intrapage-link aria-label="Search apple.com">
-					<div class="ac-gn-search-placeholder-bar">
-						<div class="ac-gn-search-placeholder-input">
-							<div class="ac-gn-search-placeholder-input-text" aria-hidden="true">
-								<div class="ac-gn-link-search ac-gn-search-placeholder-input-icon"></div>
-								<span class="ac-gn-search-placeholder">Search apple.com</span>
-							</div>
-						</div>
-						<div class="ac-gn-searchview-close ac-gn-searchview-close-small ac-gn-search-placeholder-searchview-close">
-							<span class="ac-gn-searchview-close-cancel" aria-hidden="true">Cancel</span>
-						</div>
-					</div>
-				</a>
-			</div>
-		</div>
-		<ul class="ac-gn-list">
-			<li class="ac-gn-item ac-gn-apple">
-				<a class="ac-gn-link ac-gn-link-apple" href="/" data-analytics-title="apple home" id="ac-gn-firstfocus">
-					<span class="ac-gn-link-text">Apple</span>
-				</a>
-			</li>
-			<li class="ac-gn-item ac-gn-item-menu ac-gn-store">
-				<a class="ac-gn-link ac-gn-link-store" href="/us/shop/goto/store" data-analytics-title="store">
-					<span class="ac-gn-link-text">Store</span>
-				</a>
-			</li>
-			<li class="ac-gn-item ac-gn-item-menu ac-gn-mac">
-				<a class="ac-gn-link ac-gn-link-mac" href="/mac/" data-analytics-title="mac">
-					<span class="ac-gn-link-text">Mac</span>
-				</a>
-			</li>
-			<li class="ac-gn-item ac-gn-item-menu ac-gn-ipad">
-				<a class="ac-gn-link ac-gn-link-ipad" href="/ipad/" data-analytics-title="ipad">
-					<span class="ac-gn-link-text">iPad</span>
-				</a>
-			</li>
-			<li class="ac-gn-item ac-gn-item-menu ac-gn-iphone">
-				<a class="ac-gn-link ac-gn-link-iphone" href="/iphone/" data-analytics-title="iphone">
-					<span class="ac-gn-link-text">iPhone</span>
-				</a>
-			</li>
-			<li class="ac-gn-item ac-gn-item-menu ac-gn-watch">
-				<a class="ac-gn-link ac-gn-link-watch" href="/watch/" data-analytics-title="watch">
-					<span class="ac-gn-link-text">Watch</span>
-				</a>
-			</li>
-			<li class="ac-gn-item ac-gn-item-menu ac-gn-airpods">
-				<a class="ac-gn-link ac-gn-link-airpods" href="/airpods/" data-analytics-title="airpods">
-					<span class="ac-gn-link-text">AirPods</span>
-				</a>
-			</li>
-			<li class="ac-gn-item ac-gn-item-menu ac-gn-tvhome">
-				<a class="ac-gn-link ac-gn-link-tvhome" href="/tv-home/" data-analytics-title="tv and home">
-					<span class="ac-gn-link-text">TV &amp; Home</span>
-				</a>
-			</li>
-			<li class="ac-gn-item ac-gn-item-menu ac-gn-onlyonapple">
-				<a class="ac-gn-link ac-gn-link-onlyonapple" href="/services/" data-analytics-title="only on apple">
-					<span class="ac-gn-link-text">Only on Apple</span>
-				</a>
-			</li>
-			<li class="ac-gn-item ac-gn-item-menu ac-gn-accessories">
-				<a class="ac-gn-link ac-gn-link-accessories" href="/us/shop/goto/buy_accessories" data-analytics-title="accessories">
-					<span class="ac-gn-link-text">Accessories</span>
-				</a>
-			</li>
-			<li class="ac-gn-item ac-gn-item-menu ac-gn-support">
-				<a class="ac-gn-link ac-gn-link-support" href="https://support.apple.com" data-analytics-title="support">
-					<span class="ac-gn-link-text">Support</span>
-				</a>
-			</li>
-			<li class="ac-gn-item ac-gn-item-menu ac-gn-search" role="search">
-				<a id="ac-gn-link-search" class="ac-gn-link ac-gn-link-search" href="/us/search" data-analytics-title="search" data-analytics-intrapage-link aria-label="Search apple.com"></a>
-			</li>
-			<li class="ac-gn-item ac-gn-bag" id="ac-gn-bag">
-				<div class="ac-gn-bag-wrapper">
-					<a class="ac-gn-link ac-gn-link-bag" href="/us/shop/goto/bag" data-analytics-title="bag" data-analytics-click="bag" aria-label="Shopping Bag" data-string-badge="Shopping Bag with item count : {%BAGITEMCOUNT%}">
-						<span class="ac-gn-link-text">Shopping Bag</span>
-					</a>
-					<span class="ac-gn-bag-badge" aria-hidden="true" data-analytics-title="bag" data-analytics-click="bag">
-						<span class="ac-gn-bag-badge-separator"></span>
-						<span class="ac-gn-bag-badge-number"></span>
-						<span class="ac-gn-bag-badge-unit">+</span>
-					</span>
-				</div>
-				<span class="ac-gn-bagview-caret ac-gn-bagview-caret-large"></span>
-			</li>
-		</ul>
-		<aside id="ac-gn-searchview" class="ac-gn-searchview" role="search" data-analytics-region="search">
-			<div class="ac-gn-searchview-content">
-				<div class="ac-gn-searchview-bar">
-					<div class="ac-gn-searchview-bar-wrapper">
-						<form id="ac-gn-searchform" class="ac-gn-searchform" action="/us/search" method="get">
-							<div class="ac-gn-searchform-wrapper">
-								<input id="ac-gn-searchform-input" class="ac-gn-searchform-input" type="text" aria-label="Search apple.com" placeholder="Search apple.com" autocorrect="off" autocapitalize="off" autocomplete="off" spellcheck="false" role="combobox" aria-autocomplete="list" aria-expanded="true" aria-owns="quicklinks suggestions" />
-								<input id="ac-gn-searchform-src" type="hidden" name="src" value="globalnav" />
-								<button id="ac-gn-searchform-submit" class="ac-gn-searchform-submit" type="submit" disabled aria-label="Submit Search"> </button>
-								<button id="ac-gn-searchform-reset" class="ac-gn-searchform-reset" type="reset" disabled aria-label="Clear Search">
-									<span class="ac-gn-searchform-reset-background"></span>
-								</button>
-							</div>
-						</form>
-						<button id="ac-gn-searchview-close-small" class="ac-gn-searchview-close ac-gn-searchview-close-small" aria-label="Cancel Search">
-							<span class="ac-gn-searchview-close-cancel" aria-hidden="true">
-								Cancel
-							</span>
-						</button>
-					</div>
-				</div>
-				<aside id="ac-gn-searchresults" class="ac-gn-searchresults" data-string-quicklinks="Quick Links" data-string-suggestions="Suggested Searches" data-string-noresults=""></aside>
-			</div>
-			<button id="ac-gn-searchview-close" class="ac-gn-searchview-close" aria-label="Cancel Search">
-				<span class="ac-gn-searchview-close-wrapper">
-					<span class="ac-gn-searchview-close-left"></span>
-					<span class="ac-gn-searchview-close-right"></span>
-				</span>
-			</button>
-		</aside>
-		<aside class="ac-gn-bagview" data-analytics-region="bag">
-			<div class="ac-gn-bagview-scrim">
-				<span class="ac-gn-bagview-caret ac-gn-bagview-caret-small"></span>
-			</div>
-			<div class="ac-gn-bagview-content" id="ac-gn-bagview-content">
-			</div>
-		</aside>
-	</div>
+function Frogger () {
+	this.stage 					= document.getElementById('stage');
+
+	this.streets 				= document.querySelectorAll('.street'); 	
+
+	this.frog 					= document.getElementById('frog');
+	this.frogAxis 			=  this.getFrogAxis();
+
+	//this.cars 					= this.updateCars();
+	this.carsAxis 			= 0;
+	this.carStyle 			= {
+		width 	: 10,
+		height 	: 10,
+		space 	: 10 
+	};
+	this.carsAmount 	= this.setCarsAmount();
+
+}
+
+Frogger.prototype.init = function() {
+	this.moveCars();
+};
+
+Frogger.prototype.moveCars = function() {	
+
+	var cars = this.updateCars();
+
+	for (var i = cars.length - 1; i >= 0; i--) {
+		this.carsAxis = cars[i].offsetLeft + Math.floor(Math.random()*20);
+		if ( cars[i].offsetLeft >= cars[i].parentElement.clientWidth ){
+			this.carsAxis = 0;
+		}
+		cars[i].style.left = this.carsAxis + "px";
+
+	};
+
+};
+
+Frogger.prototype.moveFrog = function(e) {
+	switch(e.which){
+		case 38: this.moveUp();
+			break;
+		case 40: this.moveDown();
+		 break;
+		case 39: this.moveRight();
+			break;
+		case 37 : this.moveLeft();
+			break;
+		default : console.log('aqui');
+	}
+};
+
+Frogger.prototype.moveUp = function() {
+	this.frogAxis.y += 1;
+	this.frog.style.bottom = this.frogAxis.y + "px";
+};
+
+Frogger.prototype.moveDown = function () {
+	this.frogAxis.y -= 1;
+	this.frog.style.bottom = this.frogAxis.y + "px";
+};
+
+Frogger.prototype.moveLeft = function () {
+	this.frogAxis.x -= 1;
+	this.frog.style.left = this.frogAxis.x + "px";
+};
+
+Frogger.prototype.moveRight = function () {
+	this.frogAxis.x += 1;
+	this.frog.style.left = this.frogAxis.x + "px";
+};
+
+Frogger.prototype.getFrogAxis = function () {
+	var
+		axis = {},
+		style = window.getComputedStyle(this.frog);
+
+		return axis = {
+			y : parseInt( style.getPropertyValue("bottom"), 10),
+			x : parseInt( style.getPropertyValue("left"), 10 )
+		}
+
+};
+
+Frogger.prototype.setCarsAmount = function() {
+	return Math.round( this.getStageWidth() / ( this.carStyle.width + this.carStyle.space ) );
+};
+
+Frogger.prototype.getStageWidth = function() {
+	return this.stage.clientWidth;
+};
+
+Frogger.prototype.loadCars = function() {
+	
+
+	for (var i = this.streets.length - 1; i >= 0; i--) {
+		var 
+		newCar = document.createElement('div');
+		newCar.setAttribute('class','car');
+
+		console.log(i + ' - ' + this.streets[i].childElementCount)
+		this.streets[i].appendChild(newCar);
+	};
+
+};
+
+Frogger.prototype.updateCars = function() {
+	return document.querySelectorAll(".car");
+};
